19838227ec6d2ff4aad6fe21efc8046b1178afa5
Aishwarya G <aishuganti96@gmail.com>
2020-07-26 02:46:15 +0530

    Update SpringCalculatorApplication.java

--- src/main/java/com/example/SpringCalculator/SpringCalculatorApplication.java
+++ src/main/java/com/example/SpringCalculator/SpringCalculatorApplication.java
@@ -15,10 +15,27 @@ public class SpringCalculatorApplication {
 		}
 		else {
 			if(numbers.startsWith("//")) {
-				Matcher m=Pattern.compile("//(.)\n(.*)").matcher(numbers);
-				m.matches();
-				String customDelimiter=m.group(1);
-				numberArray=m.group(2).split(Pattern.quote(customDelimiter));
+				String[] headerAndNumberSequence = numbers.split("\n", 2);
+				String delimiter = headerAndNumberSequence[0].substring(2);
+				if (delimiter.startsWith("[")) {
+					delimiter = delimiter.substring(1, delimiter.length() - 1);
+
+					if(delimiter.contains("][")) {
+						String delimiterStringExpression=Stream.of(delimiter.split("]\\[")).map(Pattern::quote).collect(Collectors.joining("|"));
+						numberArray=headerAndNumberSequence[1].split(delimiterStringExpression);
+					}
+					
+					else {
+						numberArray=numbers.split("\n", 2)[1].split(Pattern.quote(delimiter));
+					}
+			
+				}
+				else {
+					Matcher m=Pattern.compile("//(.)\n(.*)").matcher(numbers);
+					m.matches();
+					String customDelimiter=m.group(1);
+					numberArray=m.group(2).split(Pattern.quote(customDelimiter));
+				}				
 			}
 			else {
 				numberArray=numbers.split(",|\n");
