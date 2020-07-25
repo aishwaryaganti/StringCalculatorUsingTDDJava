0223d05a54d00581102b27665f457843ca8cfb9a
aishwaryaganti <aishwaryaganti@aishwaryas-air.dlink>
2020-07-25 23:00:10 +0530

    Add regex pattern matching to get the custom delimiters

--- src/main/java/com/example/SpringCalculator/SpringCalculatorApplication.java
+++ src/main/java/com/example/SpringCalculator/SpringCalculatorApplication.java
@@ -1,16 +1,28 @@
 package com.example.SpringCalculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SpringCalculatorApplication {
 
 	public static int add(String numbers) {
+		String[] numberArray;
 		if(numbers.isEmpty()) {
 			return 0;
 		}
 		else {
-			String[] numberArray=numbers.split(",|\n");
+			if(numbers.startsWith("//")) {
+				Matcher m=Pattern.compile("//(.)\n(.*)").matcher(numbers);
+				m.matches();
+				String customDelimiter=m.group(1);
+				numberArray=m.group(2).split(customDelimiter);
+			}
+			else {
+				numberArray=numbers.split(",|\n");
+			}
+			
 			int sum=0;
 			for(String var : numberArray) {
-				//System.out.println(var);
 				sum=sum+Integer.parseInt(var);
 			}
 			return sum;
